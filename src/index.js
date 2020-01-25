afb02853374565b564fc4eec8f222bed6950228c
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 13:15:05 -0300

    Adicao do provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './style/reset.css';
+import { Provider } from 'react-redux';
+import { Store } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
