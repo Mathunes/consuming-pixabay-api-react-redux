d7fb71cf2261aa1a542cfd27602a5b12fd3ea4cd
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 11:28:33 -0300

    Capturando texto do input

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,12 +3,21 @@ import logoPixabay from '../../assets/images/pixabay-logo.png';
 import './style.css';
 
 export default class Header extends Component {
+    state = {
+        imageSearch: '',
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.imageSearch);
+    }
+
     render() {
         return (
             <header>
                 <img src={logoPixabay} alt="logo"></img>
-                <form>
-                    <input type="text" placeholder="Buscar imagens..." />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Buscar imagens..." onChange={(e) => {this.setState({imageSearch: e.target.value})}}/>
                     <button type="submit">Buscar</button>
                 </form>
             </header>
