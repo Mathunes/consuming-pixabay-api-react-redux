962f8da79f119301fdabe5d8b7958e242d09dae0
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 13:02:59 -0300

    Adicao da conexao com a api

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://pixabay.com/api',
+    key: '12264135-54d086077e5085f21d30d2762'                            
+})
