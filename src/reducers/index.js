80a376ae2f708ec90688c434238d6aeaba346f34
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 11:51:21 -0300

    - Adicao do thunk - Adicao do store - Adicao do reducer searchImageReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,6 @@
-import 
+import { searchImageReducer } from './searchImageReducer.js';
+import { combineReducers } from 'redux';
+
+export const Reducers = combineReducers({
+    searchImageState: searchImageReducer,
+})
--- src/reducers/searchImageReducer.js
+++ src/reducers/searchImageReducer.js
@@ -5,8 +5,17 @@ const initialState = {
 export const searchImageReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SEARCH_IMAGE_STARTED':
+            return {
+                
+            }
         case 'SEARCH_IMAGE_SUCCESS':
+            return {
+                
+            }
         case 'SEARCH_IMAGE_FAILURE':
+            return {
+                
+            }
         default:
             return state;
     }
