917fc068b4bd105eb99971d6b21f5e1f77d04c57
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 11:23:35 -0300

    Adicao da logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import logoPixabay from '../../assets/images/pixabay-logo.png';
+import './style.css';
 
 export default class Header extends Component {
     render() {
         return (
             <header>
-                <img src="" alt="logo"></img>
+                <img src={logoPixabay} alt="logo"></img>
                 <form>
                     <input type="text" placeholder="Buscar imagens..." />
                     <button type="submit">Buscar</button>
