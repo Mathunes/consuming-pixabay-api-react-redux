ecf4ede774f186f9cfeba8389a80ffa595a4b430
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 13:12:57 -0300

    Adicao das acoes ao pesquisar uma imagem

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,33 @@
+import { api } from "../services/api";
+
 export function searchImage(image) {
-    
+    return dispatch => {
+        dispatch(searchImageStarted());
+
+        api.get(`${image}`)
+        .then(response => {
+            dispatch(searchImageSuccess(response));
+        })
+        .catch(error => {
+            dispatch(searchImageFailure(error.message));
+        })
+    }    
 }
+
+const searchImageStarted = () => ({
+    type: 'SEARCH_IMAGE_STARTED'
+})
+
+
+const searchImageSuccess = (data) => ({
+    type: 'SEARCH_IMAGE_SUCCESS',
+    imagesData: data
+})
+
+
+const searchImageFailure = (error) => ({
+    type: 'SEARCH_IMAGE_FAILURE',
+    payload: {
+        error
+    }
+})
