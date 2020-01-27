1ffa377b274b71f3e9fde5c51d7357e375c566d8
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-27 17:59:55 -0300

    Estilizao do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './style.css';
 
 export default class Footer extends Component {
     render() {
         return (
             <footer>
-                <p>API de imagens disponibilizada pelo <a href="https://pixabay.com/api/docs/">Pixabay</a>.</p>
+                <p>API de imagens disponibilizada pelo <a href="https://pixabay.com/api/docs/" target="_blank">Pixabay</a>.</p>
             </footer>
         )
     }
