4c4ce3e9985f54a5859121982d8cc1d44557cf0f
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-27 19:35:49 -0300

    Updates

---
+++ precache-manifest.68f42bef16d59905593a4ef327e16e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab128033e11eda852b8f5ab2cef66ad",
+    "url": "/consuming-pixabay-api-react-redux/index.html"
+  },
+  {
+    "revision": "795884c39e011ce0da46",
+    "url": "/consuming-pixabay-api-react-redux/static/css/main.9674902b.chunk.css"
+  },
+  {
+    "revision": "bf13cc070d4b03f778dc",
+    "url": "/consuming-pixabay-api-react-redux/static/js/2.0a0a945f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/consuming-pixabay-api-react-redux/static/js/2.0a0a945f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "795884c39e011ce0da46",
+    "url": "/consuming-pixabay-api-react-redux/static/js/main.2767e9c1.chunk.js"
+  },
+  {
+    "revision": "c70500ff0e5dadd7edfe",
+    "url": "/consuming-pixabay-api-react-redux/static/js/runtime-main.73fb3daa.js"
+  },
+  {
+    "revision": "bb23dff2f3d1f9434cb69a3f0564366e",
+    "url": "/consuming-pixabay-api-react-redux/static/media/loading.bb23dff2.gif"
+  }
+]);
