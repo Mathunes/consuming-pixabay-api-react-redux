61d3ba1960e311c6212d5cfac0c2587a769e4976
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-27 17:54:10 -0300

    Exibicao do footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../../store';
 import Routes from '../../routes';
+import Footer from '../Footer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
           <Routes />
+          <Footer />
       </div>
     </Provider>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>API de imagens disponibilizada pelo <a href="https://pixabay.com/api/docs/">Pixabay</a>.</p>
+            </footer>
+        )
+    }
+}
